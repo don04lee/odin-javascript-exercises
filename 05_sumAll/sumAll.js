4fc1a3e4486b799263aaa68f14850d9d90c502ad
Dong Woo Lee <don04lee@gmail.com>
2023-07-17 15:40:53 -0700

    Complete exercise 5 of odin js exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  let total = 0;
+  if(typeof start == "number" && typeof end == "number" && start > 0 && end > 0) {
+    let lower = start < end ? start : end;
+    let higher = start >= end ? start : end;
+    for(let i = lower; i <= higher; i++) {
+      total += i;
+    }
+    return total;
+  }
+  else {
+    return 'ERROR';
+  }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
