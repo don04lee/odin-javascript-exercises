ef61e637e981c81ba496a25322fd4d7bf0e06a65
Dong Woo Lee <don04lee@gmail.com>
2023-07-17 14:47:49 -0700

    Complete exercise 3 of odin js exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  let reversed = '';
+  let arr = word.split("");
+  for(let i = word.length - 1; i >= 0; i--) {
+    reversed += arr[i];
+  }
 
+  return reversed;
 };
 
 // Do not edit below this line
